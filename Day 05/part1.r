4f14a46e51ae90ce7fcbb88a908560b7e4bb24a6
Spencer A. Gray <grayspa17@gmail.com>
2022-12-06 14:31:05 -0500

    Day 5

---
+++ Day 05/part1.r
@@ -0,0 +1,41 @@
+
+f <- file('stdin')
+doneSetup <- FALSE
+open(f, open="r")
+stacks = list("", "", "", "", "", "", "", "", "")
+
+while(length(line <- readLines(f, n=1)) > 0) {
+    
+    if (doneSetup == FALSE && substring(line, 2, 2) != "1") {
+        
+        for (i in 0 : 8) {
+            num  <- (1 + i*4) + 1 
+            
+            if (substring(line, num, num) != " ") {
+                stacks[i+1] <- paste(substring(line, num, num),stacks[i+1], sep='')
+            }
+        }
+    }
+    else if (substring(line, 2, 2) == "1" || line == "") {
+        doneSetup <- TRUE
+    }
+    else { # Actually Processing Now
+                  
+        stack_numbers <- regmatches(line, gregexpr("[[:digit:]]+", line))
+        stack_numbers <- as.numeric(unlist(stack_numbers))
+        
+        for (i in 1 : stack_numbers[1]) {
+            sourceLength <- nchar(stacks[stack_numbers[2]])
+            destLength <- nchar(stacks[stack_numbers[3]])
+            
+            # Source get and remove
+            moveStack = substring(stacks[stack_numbers[2]], sourceLength, sourceLength)
+            stacks[stack_numbers[2]] <- substring(stacks[stack_numbers[2]], 1, sourceLength - 1)
+            
+            # Dest add
+            stacks[stack_numbers[3]] <- paste(stacks[stack_numbers[3]], moveStack, sep='')
+        }
+    }
+}
+
+print(stacks)
