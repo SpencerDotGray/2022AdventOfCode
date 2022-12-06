9eee5369d1a36e0e5f8406c019f59bec98d4a6c0
Spencer A. Gray <grayspa17@gmail.com>
2022-12-06 15:46:36 -0500

    Day 6

---
+++ Day 06/part2.vb
@@ -0,0 +1,39 @@
+Imports System
+
+' Day 6 Part 2
+
+Public Class Test
+    Public Shared Sub Main()
+        Dim x, y As Integer
+        Dim dsBuffer As String
+        Dim dsLength As Integer
+        Dim snippet As String
+        Dim charSnip As String
+        Dim rSnip As String
+        Dim foundIndex As Integer
+        
+        dsBuffer = Console.ReadLine()
+        dsLength = dsBuffer.Length()
+        
+        foundIndex = -1
+        
+        For num As Integer = 0 To dsLength-14 Step 1
+        snippet = dsBuffer.Substring(num, 14)
+        
+        foundIndex = num
+        FOR index As Integer = 0 To snippet.Length()-1 Step 1
+        charSnip = snippet.Substring(index, 1)
+        rSnip = snippet.Substring(0, index) & "X" & snippet.Substring(index+1)
+        If rSnip.Contains(charSnip) Then
+            foundIndex = -1
+        End If
+        Next
+        If foundIndex > 14 Then
+            Console.WriteLine(foundIndex + 14)
+            EXIT FOR
+        End If
+        Next
+        
+        
+    End Sub
+End Class
